996b9111b3d3ce28e47050477d4cd424344b3327
dkinzer <dtkinzer@gmail.com>
2015-10-11 17:16:18 +0000

    Check drupal root when in profile module.
    Ref: https://github.com/NuCivic/internal/issues/732

--- test/boot.php
+++ test/boot.php
@@ -10,8 +10,15 @@ use Drupal\Driver\Cores\Drupal7;
 require 'vendor/autoload.php';
 
 $dir = explode('/sites/', getcwd());
+
+// No split may mean we are in a profiles folder:
+if (count($dir) == 1) {
+  $dir = explode('/profiles/', getcwd());
+}
+
 // Path to Drupal.
 $path = $dir[0];
+
 // Host.
 $uri = 'http://localhost';
 
