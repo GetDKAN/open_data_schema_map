1c790db8b5eaae8b32a6db5b8657961e2dc27aa3
Frank Carey <frank@devinci.io>
2016-02-19 18:39:14 -0500

    Update dkan-module-init.sh
    update to latest dkan-module-init.sh
    update to latest dkan 7.x-1.x

--- dkan-module-init.sh
+++ dkan-module-init.sh
@@ -3,7 +3,7 @@
 DKAN_MODULE="open_data_schema_map"
 
 # DKAN branch to use
-DKAN_BRANCH="dev-dkan-ahoy-smarter"
+DKAN_BRANCH="7.x-1.x"
 
 COMPOSER_PATH=".composer/vendor/bin"
 
@@ -17,7 +17,7 @@ fi
 wget -O /tmp/dkan-init.sh https://raw.githubusercontent.com/NuCivic/dkan/$DKAN_BRANCH/dkan-init.sh
 
 # Make sure the download was at least successful.
-if [ $? -ne 0] ; then
+if [ $? -ne 0 ] ; then
   echo ""
   echo "[Error] Failed to download the dkan-init.sh script from github dkan. Branch: $DKAN_BRANCH . Perhaps someone deleted the branch?"
   echo ""
@@ -27,7 +27,7 @@ fi
 #Only stop on errors starting now..
 set -e
 # OK, run the script.
-bash /tmp/dkan-init.sh $DKAN_MODULE $@ --skip-reinstall
+bash /tmp/dkan-init.sh $DKAN_MODULE $@ --skip-reinstall --branch=$DKAN_BRANCH
 ahoy dkan module-link $DKAN_MODULE
 ahoy dkan module-make $DKAN_MODULE
 ahoy dkan reinstall
