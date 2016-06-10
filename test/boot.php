66e04f73b5b24d39d5b6fe899cb621eb2f3607b2
Teofilo Sibileau <teo.sibileau@gmail.com>
2016-06-10 17:52:56 -0300

    #CIVIC-2719 adds support for dkan archives to dkan-module-init

--- test/boot.php
+++ test/boot.php
@@ -7,22 +7,17 @@
 use Drupal\Driver\DrupalDriver;
 use Drupal\Driver\Cores\Drupal7;
 
-require 'vendor/autoload.php';
-
-$dir = explode('/sites/', getcwd());
-
-// No split may mean we are in a profiles folder:
-if (count($dir) == 1) {
-  $dir = explode('/profiles/', getcwd());
-}
+require __DIR__ . '/../vendor/autoload.php';
 
 // Path to Drupal.
-$path = $dir[0];
+$dir = implode('/', array(__DIR__, '..', '..', 'docroot'));
 
 // Host.
-$uri = 'http://localhost';
+$uri = getenv('DKAN_WEB_1_ENV_VIRTUAL_HOST') ? 'http://' . getenv('DKAN_WEB_1_ENV_VIRTUAL_HOST') : 'http://127.0.0.1:8888';
 
-$driver = new DrupalDriver($path, $uri);
+var_dump($uri);
+
+$driver = new DrupalDriver($dir, $uri);
 $driver->setCoreFromVersion();
 
 // Bootstrap Drupal.
