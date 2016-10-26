ad7eb0faf6860540479a07de5e0333ea811362a4
Mariano Carballal <marianocarballal@gmail.com>
2016-10-26 14:44:11 -0300

    Remove not needed retriever

--- modules/open_data_schema_dcat/dcatValidator.php
+++ modules/open_data_schema_dcat/dcatValidator.php
@@ -61,7 +61,6 @@ class validate {
 
   public function processAll() {
     $this->getDataRDF();
-    $retriever = new UriRetriever;
     $this->getIdentifiers();
     $this->validated = array();
     foreach ($this->identifiers as $id) {
